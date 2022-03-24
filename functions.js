a7928779a9a83696fc9e1bc8cd32ceff814279cf
David Daw <daviddaw@gmail.com>
2022-03-24 09:36:50 -0700

    Add addTwoNumbers, sayHello and buildCar functions that pass all tests.

--- functions.js
+++ functions.js
@@ -1,9 +1,33 @@
 // functions.js
-function addTwoNumbers() {
-}
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+};
 
-module.exports = {
-    addTwoNumbers,
-    // sayHello,
-    // buildCar
-}
+function sayHello(name) {
+  if (name === 'will' || name === 'Will') {
+    return `No more testing ${name}!`
+  } else {
+    return `Hi there ${name}!`
+  }
+};
+
+var result = {};
+
+//This function builds an object with color and type keys and produces an empty object if you haven't defined those things.
+//feels like it does all those wrong though so it might be worth cleaning up the code here.
+function buildCar(color, type) {
+  if (color === undefined && type === undefined) {
+    return {};
+  } else
+{  result.color = color;
+  result.type = type;
+  return result; }
+  };
+
+  console.log(buildCar('red', 'good'));
+
+  module.exports = {
+      addTwoNumbers,
+      sayHello,
+      buildCar
+  };
